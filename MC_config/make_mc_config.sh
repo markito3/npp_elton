2241a43667bd467bff4631fb80153083fc40c200
Mark Ito <marki@jlab.org>
2020-04-23 15:13:01 -0400

    Edit a template to make the two MCW config files.

--- MC_config/make_mc_config.sh
+++ MC_config/make_mc_config.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 replacements=$1
 . $replacements
-script=s^\<tag\>^$tag^
-script=${script}\;s^\<output_dir\>^$output_dir^
-script=${script}\;s^\<npp_elton_dir\>^$npp_elton_dir^
-script=${script}\;s^\<generator_config_file\>^$generator_config_file^
-sed $script MC.config.template
+script=''
+script=$script\;s#\<tag\>#$tag#
+script=$script\;s#\<output_dir\>#$output_dir#
+script=$script\;s#\<npp_elton_dir\>#$npp_elton_dir#
+script=$script\;s#\<generator_config_file\>#$generator_config_file#
+sed $script $NPP/MC_config/MC.config.template
