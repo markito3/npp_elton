ec49968724d0e09b34d97c8cfda2b27bb2b8296f
markito3 <marki@jlab.org>
2020-05-01 15:23:55 -0400

    Rewrite the config file scheme for making MC.config files.

--- MC_config/make_mc_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-replacements=$1
-. $replacements
-script=''
-script=$script\;s#\<tag\>#$tag#
-script=$script\;s#\<output_dir\>#$output_dir#
-script=$script\;s#\<npp_elton_dir\>#$npp_elton_dir#
-script=$script\;s#\<generator_config_file\>#$generator_config_file#
-sed $script $NPP/MC_config/MC.config.template
